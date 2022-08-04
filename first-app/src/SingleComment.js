34b043d311c116e94c079a576e2fc69174a21334
abhinavxox <abhinavxox@gmail.com>
2022-08-04 15:08:18 +0545

    <day-3>:[props completed]

--- first-app/src/SingleComment.js
+++ first-app/src/SingleComment.js
@@ -1,19 +1,18 @@
 import React from "react";
-import a1 from './image/a.jpg';
 
-const SingleComment = () => {
+const SingleComment = (props) => {
     return (
         <div className="comment">
             <a className="avatar">
-                <img src={a1} />
+                <img src={props.image} />
             </a>
             <div className="content">
-                <a className="author">Matt</a>
+                <a className="author">{props.name}</a>
                 <div className="metadata">
-                    <span className="date">Today at 5:42PM</span>
+                    <span className="date">{props.date}</span>
                 </div>
                 <div className="text">
-                    How artistic!
+                    {props.text}
                 </div>
                 <div className="actions">
                     <a className="reply">Reply</a>
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SingleComment from "./SingleComment";
+import image1 from './image/a.jpg';
+import image2 from './image/b.png';
+import image3 from './image/c.jpg';
+import UserCard from "./UserCard";
 
 const App = () => {
   const text = { text: 'Click' }
@@ -11,10 +15,31 @@ const App = () => {
     //   <button style={{ backgroundColor: 'red', color: 'white' }}>{text.text}</button>
     // </form>
 
-    <div className="ui comments" >
-      <SingleComment />
-      <SingleComment />
-      <SingleComment />
+    <div className="ui comments" style={{ padding: '5%' }}>
+      <UserCard>
+        <SingleComment
+          name="Alex"
+          date="Today at 4:50pm"
+          text="It's amazing!"
+          image={image1}
+        />
+      </UserCard>
+      <UserCard>
+        <SingleComment
+          name="Sarah"
+          date="Today at 4:53pm"
+          text="Yes it is."
+          image={image2}
+        />
+      </UserCard>
+      <UserCard>
+        <SingleComment
+          name="Johnny"
+          date="Today at 4:56pm"
+          text="How wonderful."
+          image={image3}
+        />
+      </UserCard>
     </div>
   )
 }
