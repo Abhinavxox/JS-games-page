fcc044998d4adc5673033e80e3f0d424348ddeae
abhinavxox <abhinavxox@gmail.com>
2022-08-04 17:56:24 +0545

    <day-3>:[Leared about class based component]

---
+++ first-app/src/HemishpereDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HemisphereDisplay = () => {
+    return (
+        <div>
+            Hey you are in Northern Hemisphere!
+        </div>
+    )
+}
+
+export default HemisphereDisplay;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -6,42 +6,65 @@ import image2 from './image/b.png';
 import image3 from './image/c.jpg';
 import UserCard from "./UserCard";
 
-const App = () => {
-  const text = { text: 'Click' }
-  return (
-    // <form>
-    //   <label classNameName="label" htmlFor="name">Email :</label>
-    //   <input id="name" type="text" />
-    //   <button style={{ backgroundColor: 'red', color: 'white' }}>{text.text}</button>
-    // </form>
+//functional component
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (error) => console.log(error)
+//   );
+//   return (
+//     // <form>
+//     //   <label classNameName="label" htmlFor="name">Email :</label>
+//     //   <input id="name" type="text" />
+//     //   <button style={{ backgroundColor: 'red', color: 'white' }}>{text.text}</button>
+//     // </form>
 
-    <div className="ui comments" style={{ padding: '5%' }}>
-      <UserCard>
-        <SingleComment
-          name="Alex"
-          date="Today at 4:50pm"
-          text="It's amazing!"
-          image={image1}
-        />
-      </UserCard>
-      <UserCard>
-        <SingleComment
-          name="Sarah"
-          date="Today at 4:53pm"
-          text="Yes it is."
-          image={image2}
-        />
-      </UserCard>
-      <UserCard>
-        <SingleComment
-          name="Johnny"
-          date="Today at 4:56pm"
-          text="How wonderful."
-          image={image3}
-        />
-      </UserCard>
-    </div>
-  )
+//     // <div className="ui comments" style={{ padding: '5%' }}>
+//     //   <UserCard>
+//     //     <SingleComment
+//     //       name="Alex"
+//     //       date="Today at 4:50pm"
+//     //       text="It's amazing!"
+//     //       image={image1}
+//     //     />
+//     //   </UserCard>
+//     //   <UserCard>
+//     //     <SingleComment
+//     //       name="Sarah"
+//     //       date="Today at 4:53pm"
+//     //       text="Yes it is."
+//     //       image={image2}
+//     //     />
+//     //   </UserCard>
+//     //   <UserCard>
+//     //     <SingleComment
+//     //       name="Johnny"
+//     //       date="Today at 4:56pm"
+//     //       text="How wonderful."
+//     //       image={image3}
+//     //     />
+//     //   </UserCard>
+//     // </div>
+
+//     <div>
+//       Helloworld!
+//     </div>
+//   )
+// }
+
+//class based component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+    return (
+      <div>
+        You are in Northern Hemisphere!
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
