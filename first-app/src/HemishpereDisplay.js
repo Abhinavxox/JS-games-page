135a31a66812a44fb6425c6efc96e390cd8cecb8
abhinavxox <abhinavxox@gmail.com>
2022-08-11 16:43:55 +0545

    <day-8>;[learned advanced styling]

--- first-app/src/HemishpereDisplay.js
+++ first-app/src/HemishpereDisplay.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Southern from './image/southern.jpg'
+import Northern from './image/northern.jpg'
 
 const HemisphereDisplay = ({ latitude }) => {
     const hem = latitude > 0 ? "Northern" : "Southern"
+    const pic = latitude > 0 ? Northern : Southern
     return (
         <div>
+            <img src={pic} alt="" />
             Hey you are in {hem} hemisphere!
         </div>
     )
