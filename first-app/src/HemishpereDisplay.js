f9e25a1d34fe938590c3283ea0580df6776770ea
abhinavxox <abhinavxox@gmail.com>
2022-08-23 22:08:45 +0545

    :

--- first-app/src/HemishpereDisplay.js
+++ first-app/src/HemishpereDisplay.js
@@ -1,14 +1,33 @@
 import React from "react";
+import './Hemisphere.css';
 import Southern from './image/southern.jpg'
 import Northern from './image/northern.jpg'
 
+const hemisphereConfig = {
+    Northern: {
+        text: 'it is northern hemisphere',
+        pic: Northern
+    },
+    Southern: {
+        text: 'it is southern hemisphere',
+        pic: Southern
+    }
+}
+
 const HemisphereDisplay = ({ latitude }) => {
     const hem = latitude > 0 ? "Northern" : "Southern"
-    const pic = latitude > 0 ? Northern : Southern
     return (
-        <div>
-            <img src={pic} alt="" />
-            Hey you are in {hem} hemisphere!
+        <div className={hem}>
+            <div className="ui raised text container segment">
+                <div className="image">
+                    <img src={hemisphereConfig[hem].pic} alt="" />
+                </div>
+                <div className="ui teal bottom attached label">
+                    Hey you are in {hemisphereConfig[hem].text} hemisphere!
+                </div>
+            </div>
+
+
         </div>
     )
 }
