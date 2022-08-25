4bc90312d4564b26bb9fb815212d2da4a0fe62c0
abhinavxox <abhinavxox@gmail.com>
2022-08-25 08:39:47 +0545

    <api requesting>:[completed and rendered]

--- image-list/src/components/ImageList.js
+++ image-list/src/components/ImageList.js
@@ -1,21 +1,14 @@
 import React from "react";
 
 const ImageList = (props) => {
-    const imagess = props.images;
+    const images = props.images.map(
+        (image) => {
+            return <img key={image.id} src={image.webformatURL} alt="" />
+        }
+    )
     return (
         <div className="ui segment">
-            <div className="ui relaxed divided list">
-                {imagess.map(image => {
-                    return (
-                        <div key={image.id} className="item">
-                            <div className="ui tiny image">
-                                <img src={image.webformatURL} alt={image.tags} />
-                            </div>
-                        </div>
-                    )
-                }
-                )}
-            </div>
+            {images}
         </div>
     )
 }
