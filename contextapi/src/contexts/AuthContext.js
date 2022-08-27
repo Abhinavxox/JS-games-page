61cb347d00961221a5ac0913a0f0e00c85e142aa
abhinavxox <abhinavxox@gmail.com>
2022-08-27 18:06:56 +0545

    <context>:[completed]

---
+++ contextapi/src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends React.Component {
+    state = {
+        isLoggedIn: true
+    }
+
+    changeAuthStatus = () => {
+        this.setState({ isLoggedIn: !this.state.isLoggedIn })
+    }
+
+    render() {
+        return (
+            <AuthContext.Provider value={{ ...this.state, changeAuthStatus: this.changeAuthStatus }}>
+                {this.props.children}
+            </AuthContext.Provider>
+        )
+    }
+}
+
+export default AuthContextProvider;
