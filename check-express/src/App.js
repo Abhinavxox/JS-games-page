f186b5954e8b75e5168ba0ea764a77e9a85a827f
Abhinavxox <abhinavxox@gmail.com>
2023-02-19 13:43:43 +0530

    <19/0>:[]

--- check-express/src/App.js
+++ check-express/src/App.js
@@ -1,35 +1,47 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
-
-const fetchFromServer = async () => {
-  const requestURL = `http://localhost:5000/a`;
-  const response = await axios.get(requestURL);
-  return response.data;
-};
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
-  const RenderUsers = async () => {
-    const data = await fetchFromServer();
-    console.log(data);
-    return (
-      <>
-        {/* {data.users.map((data) => (
-          <div key={data.name}>
-            {data.name} : {data.password}
-          </div>
-        ))} */}
-      </>
-    );
-  };
+  const [Users, setUsers] = useState();
+  const [Loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const requestURL = `http://localhost:5000/a`;
+    axios.get(requestURL).then((response) => {
+      setUsers(response.data);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div className="ui segment">
       <Form />
       <div className="ui segment">
         <h1>Registered Users:</h1>
-        <div className="users list">
-          <RenderUsers />
-        </div>
+        {Loading ? (
+          <HashLoader
+            color="#000000"
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        ) : (
+          <div className="users list">
+            <ul>
+              {Users &&
+                Users.users.map((data) => {
+                  return (
+                    <ul key={data.name}>
+                      <li>{data.name}</li>
+                      <li>{data.password}</li>
+                    </ul>
+                  );
+                })}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
